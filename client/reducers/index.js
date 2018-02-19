cc99cf4f65a6db1bf4f2bcbf30afc6a73bfac22d
Kyle Yee <20980584+ShifuYee@users.noreply.github.com>
2018-02-18 20:36:55 -0500

    added rootReducer file

---
+++ client/reducers/index.js
@@ -0,0 +1,12 @@
+'use strict';
+// import { combineReducers } from 'redux';
+
+const initialState = {};
+
+const rootReducer = function (state = initialState, action) {
+  switch (action.type) {
+    default: return state;
+  }
+};
+
+export default rootReducer;
